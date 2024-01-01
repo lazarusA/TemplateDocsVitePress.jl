23ebf14ad048fac3a81e0635d1659443260c2870
Lazaro Alonso <lazarus.alon@gmail.com>
2024-01-01 01:06:17 +0100

    ci again

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,6 @@
 using Documenter
 using DocumenterVitepress
 
-#cp(joinpath(@__DIR__,"deploy"), joinpath(@__DIR__,"docs/"), force=true)
 
 makedocs(; sitename="TemplateDocsVitepress", authors="Lazaro Alonso",
     modules=[DocumenterVitepress],
@@ -9,8 +8,5 @@ makedocs(; sitename="TemplateDocsVitepress", authors="Lazaro Alonso",
     format=DocumenterVitepress.MarkdownVitepress(),
     draft=false,
     source="src",
-    build=joinpath(@__DIR__, "docs/dev") # dev version
+    build=joinpath(@__DIR__, "docs_site/")
     )
-
-deploydocs(; repo="github.com/lazarusA/TemplateDocsVitePress.jl.git", push_preview=true,
-    target="docs", devurl=".", devbranch="master")
