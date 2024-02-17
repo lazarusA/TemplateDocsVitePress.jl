354b242d8bb23bbaf54713fecc0108355ce53956
Lazaro Alonso <lazarus.alon@gmail.com>
2024-02-17 01:13:20 +0100

    up template structure

--- docs/make.jl
+++ docs/make.jl
@@ -1,12 +1,15 @@
 using Documenter
 using DocumenterVitepress
+using TemplateDocsVitePress
 
-
-makedocs(; sitename="TemplateDocsVitepress", authors="Lazaro Alonso",
-    modules=[DocumenterVitepress],
+makedocs(; sitename="TemplateDocsVitePress", authors="Lazaro Alonso",
+    modules=[TemplateDocsVitePress],
     checkdocs=:all,
     format=DocumenterVitepress.MarkdownVitepress(),
     draft=false,
     source="src",
-    build=joinpath(@__DIR__, "docs_site/")
+    build=joinpath(@__DIR__, "docs_site/docs")
     )
+
+deploydocs(; repo="github.com/lazarusA/TemplateDocsVitePress.git",
+    target="docs_site", devbranch="master")
