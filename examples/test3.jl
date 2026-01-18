3720e84b8e8dba5b863105c71aea894d2b6ac872
Lazaro Alonso <lazarus.alon@gmail.com>
2026-01-18 16:28:53 +0100

    dont

--- examples/test3.jl
+++ examples/test3.jl
@@ -1,17 +1,1 @@
 # # Basic examples
-
-#=
-```@cardmeta
-Title = "Basic examples 2"
-Description = "Basic examples of GeoMakie 3"
-Cover = fig
-```
-=#
-
-using GLMakie # you could use GLMakie too
-
-# Surface example
-fig = Figure(; size = (800,600))
-ax = Axis(fig[1,1])
-heatmap!(ax, 1..2, 1..2, rand(10,10))
-fig
